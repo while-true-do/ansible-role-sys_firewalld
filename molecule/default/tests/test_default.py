d068a8f0fd2655c0f9d5d56fac1f244099a015ec
daniel-wtd <daniel@while-true-do.io>
2019-05-24 21:22:25 +0200

    fix: docker does not support firewalling

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,10 +8,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_firewalld_pkg(host):
     pkg = host.package('firewalld')
+
     assert pkg.is_installed
 
 
 def test_firewalld_srv(host):
-    srv = host.service('firewalld')
-    assert srv.is_running
-    assert srv.is_enabled
+    vrt = host.ansible("setup")["ansible_facts"]["ansible_virtualization_type"]
+    if vrt != 'docker':
+        srv = host.service('firewalld')
+
+        assert srv.is_running
+        assert srv.is_enabled
